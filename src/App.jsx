b695a5f5dbe4b7727cf80f23d9fff700acaad521
scalaless <dmitrykanygin1488@yandex.ru>
2024-11-27 18:04:33 +0500

    +-

--- src/App.jsx
+++ src/App.jsx
@@ -9,13 +9,25 @@ const inputClick = () => {
 const helpText = "help text"
 
 const App = () => {
+  const [count, setCount] = useState(0)
+
+  const increment = () => {
+    setCount((prev) => prev + 1)
+
+  }
+  const decrement = () => {
+    setCount((prev) => prev - 1)
+  }
 
   return (
     <div>
-      <Header />
+      <Header count={count}/>
       <Logo image={image} />
       <br/>
       <input placeholder={helpText} onClick={inputClick}></input>
+      <br/>
+      <button onClick={increment}>Добавить 1</button>
+      <button onClick={decrement}>Убрать 1</button>
     </div>)
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
-const Header = () => {
+const Header = ({count}) => {
     return (
-      <header>site up</header> 
+      <header>site up {count}</header> 
     )
   }
 
